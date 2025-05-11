0bc1a9061b6a907ffff6c96b01e8c66c379d6dcf
aidenkrew <ajkrueger@ucsd.edu>
2025-05-11 15:59:27 -0700

    Create JS file for testing JSDocs

---
+++ src/jsdoc-test.js
@@ -0,0 +1,9 @@
+/**
+ * Adds two numbers.
+ * @param {number} a - First number
+ * @param {number} b - Second number
+ * @returns {number} Sum of a and b
+ */
+function add(a, b) {
+  return a + b;
+}
