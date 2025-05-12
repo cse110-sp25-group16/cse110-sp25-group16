161fd64b591f2d25cef64ff7aa502ccf573f1e12
Lyka717 <acabusi@ucsd.edu>
2025-05-11 23:08:34 -0700

    admin: Fixed, test expected to pass

--- codacy-check.js
+++ codacy-check.js
@@ -1,2 +1,2 @@
 const status = "Codacy is working!";
-console.log();
+console.log(status);
--- sum.js
+++ sum.js
@@ -1,6 +1,12 @@
+/**
+ * This function takes in two numbers and returns their sum.
+ * @param {int} a First input number
+ * @param {int} b Second input number
+ * @returns {int} The sum of the two numbers
+ */
 function sum(a, b) {
-  return a - b;
+  return a + b;
 }
 export default function sum(a, b) {
-  return a - b;
+  return a + b;
 }
