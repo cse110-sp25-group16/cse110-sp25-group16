c7243713a82168712bde7da60095298849b9ab74
Tyler <tylerhoang456123@gmail.com>
2025-05-12 00:15:08 -0700

    fix: modify eslint run test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,13 @@ export default defineConfig([
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
+    ignores: [
+      'testcode.js',
+      'docs/**',
+      'node_modules/**',
+      'sum.test.js',
+      'sum.js'
+    ]
   },
   {
     files: ['**/*.{js,mjs,cjs}'],
