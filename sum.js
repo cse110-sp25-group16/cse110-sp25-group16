515cb884e0c8c88e903efac25c13955609522899
Lyka717 <acabusi@ucsd.edu>
2025-05-11 23:35:28 -0700

    admin: Tests expected to pass

--- sum.js
+++ sum.js
@@ -1,6 +1,12 @@
+/**
+ * This function takes two numbers and return their sum.
+ * @param {int} a First input number
+ * @param {int} b Second input number
+ * @returns {int} Sum of the two numbers
+ */
 function sum(a, b) {
-  return a - b;
+  return a + b;
 }
 export default function sum(a, b) {
-  return a - b;
+  return a + b;
 }
