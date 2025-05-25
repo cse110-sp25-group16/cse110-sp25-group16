e8fbbb17d35443e962572214f542295f3ccaeb43
riasinghania <73354668+riasinghania@users.noreply.github.com>
2025-05-25 14:20:29 -0700

    Testing out Navigation Bar creation with placeholder Tarot, MBTI, and Horoscope pages.

---
+++ source/frontend/navbar.js
@@ -0,0 +1,16 @@
+class NavBar extends HTMLElement {
+    constructor() {
+      super();
+      this.innerHTML = `
+        <nav>
+          <ul class="navbar">
+            <li><a href="tarot.html">Tarot</a></li>
+            <li><a href="horoscope.html">Horoscope</a></li>
+            <li><a href="mbti.html">MBTI</a></li>
+          </ul>
+        </nav>
+      `;
+    }
+  }
+  
+  customElements.define('nav-bar', NavBar);
