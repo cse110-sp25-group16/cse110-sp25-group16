b1e4f85b073d4ac20ac46e413e320fb08d645e4d
LeoJuJu05090822 <103226676+LeoJuJu05090822@users.noreply.github.com>
2025-05-26 04:07:26 -0700

    Add the Card.test.js File Changed the expprt of Card.js from defult to module. has the right type All following Test case are passed: 1. contains meaningful card entries 2. has no duplicates 3.  has valid image references and numerals 4.  provides all required properties on each card

--- source/backend/Card.js
+++ source/backend/Card.js
@@ -1,4 +1,4 @@
-var cardDictionary = [
+var cardDictionary = 
   {
     0: {
       name: 'The Fool',
@@ -2200,8 +2200,7 @@ var cardDictionary = [
         "The figure calls for no special description the face is rather dark, suggesting also courage, but somewhat lethargic in tendency. The bull's head should be noted as a recurrent symbol on the throne. The sign of this suit is represented throughout as engraved or blazoned with the pentagram, typifying the correspondence of the four elements in human nature and that by which they may be governed. In many old Tarot packs this suit stood for current coin, money, deniers. I have not invented the substitution of pentacles and I have no special cause to sustain in respect of the alternative. But the consensus of divinatory meanings is on the side of some change, because the cards do not happen to deal especially with questions of money.",
       numeral: '14',
     },
-  },
-];
+  };
 
 // async () => {
 //     const response = await fetch('./output.json');
@@ -2335,4 +2334,4 @@ class Card {
   }
 }
 
-export default Card;
+module.exports = cardDictionary;
