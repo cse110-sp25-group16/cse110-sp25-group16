a5f8e76ca2cb11926a8c38cf5422e8ee7dd20eac
LeoJuJu05090822 <103226676+LeoJuJu05090822@users.noreply.github.com>
2025-05-26 03:57:25 -0700

    Unit test for Card.js

--- sum.js
+++ sum.js
@@ -1,9 +1,11 @@
 /**
- * Function decription
+ * Function description
  * @param {int} a
  * @param {int} b
- * @returns {int} Returns the sum of the the two input numbers
+ * @returns {int} Returns the sum of the two input numbers
  */
 function sum(a, b) {
   return a + b;
 }
+
+module.exports = sum;
