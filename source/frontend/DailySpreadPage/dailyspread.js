7903d4e873afde3e3a4001daf8301c31f0b572cb
Lyka717 <acabusi@ucsd.edu>
2025-05-26 11:57:38 -0700

    Add: added localStorage saving

--- source/frontend/DailySpreadPage/dailyspread.js
+++ source/frontend/DailySpreadPage/dailyspread.js
@@ -1,13 +1,10 @@
 import CardDeck from '../../backend/CardDeck.js';
+import Card from '../../backend/Card.js';
 
 /**
- * Upon page load, whenever the user changes the amount of cards to draw via dropdown menu,
- * the init function is called.
+ * Upon page load, call the init() function
  */
-window.addEventListener('DOMContentLoaded', () => {
-   const selectedAmnt = document.querySelector('#card-amount');
-   selectedAmnt.addEventListener('change', init);
-  });
+window.addEventListener('DOMContentLoaded', init);
 
 /**
  * This function serves as the main function for the Dail spread page.
@@ -15,41 +12,66 @@ window.addEventListener('DOMContentLoaded', () => {
 async function init() {
   /*Selects grid-container in html and clears the cards loaded from previous draw*/
   const grid = document.querySelector('.cards-container');
-  grid.innerHTML = '';
+  const selectedAmnt = document.querySelector('#card-amount');
+
+  const currDate = new Date().toLocaleDateString();
+  const savedCards = JSON.parse(localStorage.getItem('dailyCards'));
+
+  let pulledCards;
+
+  /**
+   * Checks if have cards already drawn and saved today and uses those cards to display.
+   * Otherwise, draw (max) cards, assign a today's date, and save to localStorage using date as key.
+   */
+  if (savedCards && savedCards.date === currDate) {
+    pulledCards = savedCards.cards.map((id) => new Card(id, false));
+  } else {
+    const deck = new CardDeck();
+    deck.fillDeck();
+    deck.shuffle();
+    pulledCards = deck.drawing(10);
+
+    localStorage.setItem(
+      'dailyCards',
+      JSON.stringify({
+        date: currDate,
+        cards: pulledCards.map((card) => card.id),
+      })
+    );
+  }
 
   /*Selects cards and amount drawn depends on what option is picked */
-  const selectedAmnt = document.querySelector('#card-amount');
-  const deck = new CardDeck();
-  deck.fillDeck();
-  deck.shuffle();
-  const pulledCards = deck.drawing(parseInt(selectedAmnt.value));
+  selectedAmnt.addEventListener('change', (event) => {
+    /*Clears amount of card displayed from previous selection */
+    grid.innerHTML = '';
 
-  /* (**BUG**) attempt to save to local storage */
-  localStorage.setItem('pulledCards', JSON.stringify(pulledCards));
+    /*For each card drew, create a card webcomponent to then append and display */
+    for (let i = 0; i < event.target.value; i++) {
+      const cardElement = document.createElement('card-component');
 
-  /*For each card drew, create a card webcomponent to then append and display */
-  for (let i = 0; i < pulledCards.length; i++) {
-    const cardElement = document.createElement('card-component');
-
-    cardElement.setAttribute('image', `/source/cards/${pulledCards[i].getImg()}`);
-    cardElement.setAttribute('name', pulledCards[i].getCardName());
-    cardElement.setAttribute('arcana', pulledCards[i].getArcana());
-    cardElement.setAttribute('suit', pulledCards[i].getSuit());
-    cardElement.setAttribute(
-      'uprightMeanings',
-      JSON.stringify(pulledCards[i].getUprightMeanings())
-    );
-    cardElement.setAttribute(
-      'reversedMeanings',
-      JSON.stringify(pulledCards[i].getReversedMeaning())
-    );
-    cardElement.setAttribute(
-      'keywords',
-      JSON.stringify(pulledCards[i].getKeywords())
-    );
-    cardElement.setAttribute('symbolism', pulledCards[i].getSymbolism());
-    cardElement.setAttribute('description', pulledCards[i].getDescription());
-    cardElement.setAttribute('numeral', pulledCards[i].getNumeral());
-    grid.appendChild(cardElement);
-  }
+      cardElement.setAttribute(
+        'image',
+        `/source/cards/${pulledCards[i].getImg()}`
+      );
+      cardElement.setAttribute('name', pulledCards[i].getCardName());
+      cardElement.setAttribute('arcana', pulledCards[i].getArcana());
+      cardElement.setAttribute('suit', pulledCards[i].getSuit());
+      cardElement.setAttribute(
+        'uprightMeanings',
+        JSON.stringify(pulledCards[i].getUprightMeanings())
+      );
+      cardElement.setAttribute(
+        'reversedMeanings',
+        JSON.stringify(pulledCards[i].getReversedMeaning())
+      );
+      cardElement.setAttribute(
+        'keywords',
+        JSON.stringify(pulledCards[i].getKeywords())
+      );
+      cardElement.setAttribute('symbolism', pulledCards[i].getSymbolism());
+      cardElement.setAttribute('description', pulledCards[i].getDescription());
+      cardElement.setAttribute('numeral', pulledCards[i].getNumeral());
+      grid.appendChild(cardElement);
+    }
+  });
 }
