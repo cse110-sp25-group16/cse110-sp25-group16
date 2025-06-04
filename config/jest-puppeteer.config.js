23d486487d2faffce1735bfefde3890f2422650f
dustinnguyen012 <dun012@ucsd.edu>
2025-06-04 08:23:42 +0000

    Run live-server manually in CI pipeline for E2E tests

--- config/jest-puppeteer.config.js
+++ config/jest-puppeteer.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   launch: {
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  },
-  server: {
-    command: 'npx live-server . --port=8080 --quiet',
-    port: 8080,
-    launchTimeout: 10000,
-    debug: true,
-  },
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  }
 };
+w
