36e63a93b6b6e81da35a5a8a4bfe2455d754e105
Nakiti <nikhilakiti91@gmail.com>
2025-06-07 01:45:53 -0700

    added active state

--- source/frontend/ArchivePage/archive.js
+++ source/frontend/ArchivePage/archive.js
@@ -2,7 +2,9 @@
 window.addEventListener('DOMContentLoaded', init);
 
 
-
+/**
+ * Gets archived tarot/horoscope for particular date based on active state 
+ */
 async function init() {
    let active = "tarot"
    let date = Date.now()
@@ -11,16 +13,32 @@ async function init() {
    const horoscopeButton = document.getElementById("horoscope")
    const dateInput = document.getElementById("date")
 
+   const updateActiveButton = () => {
+      tarotButton.classList.remove("active")
+      horoscopeButton.classList.remove("active")
+
+      if (active === "tarot") {
+         tarotButton.classList.add("active")
+      } else {
+         horoscopeButton.classList.add("active")
+      }
+   };
+
    tarotButton.addEventListener("click", () => {
       active = "tarot"
+      updateActiveButton()
+      getArchived(active, date)
    })
 
    horoscopeButton.addEventListener("click", () => {
       active = "horoscope"
+      updateActiveButton()
+      getArchived(active, date)
    })
 
    dateInput.addEventListener("change", (e) => {
       date = e.target.value
+      getArchived(active, date)
    })
 
 }
