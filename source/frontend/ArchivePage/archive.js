08e0af24cfabc06804383a0eda735cac37961f64
Nakiti <nikhilakiti91@gmail.com>
2025-06-07 00:45:57 -0700

    set up init archive page

---
+++ source/frontend/ArchivePage/archive.js
@@ -0,0 +1,30 @@
+
+window.addEventListener('DOMContentLoaded', init);
+
+
+
+async function init() {
+   let active = "tarot"
+   let date = Date.now()
+
+   const tarotButton = document.getElementById("tarot")
+   const horoscopeButton = document.getElementById("horoscope")
+   const dateInput = document.getElementById("date")
+
+   tarotButton.addEventListener("click", () => {
+      active = "tarot"
+   })
+
+   horoscopeButton.addEventListener("click", () => {
+      active = "horoscope"
+   })
+
+   dateInput.addEventListener("change", (e) => {
+      date = e.target.value
+   })
+
+}
+
+function getArchived(active, date) {
+   
+}
