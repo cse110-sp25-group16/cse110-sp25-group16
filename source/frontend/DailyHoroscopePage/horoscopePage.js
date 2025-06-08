a53637c47376035394e2d9663b83edd3048f9c68
ominjoo <o.minjoo14@gmail.com>
2025-06-07 21:03:30 -0700

    fixed linting + html validator errors

--- source/frontend/DailyHoroscopePage/horoscopePage.js
+++ source/frontend/DailyHoroscopePage/horoscopePage.js
@@ -4,30 +4,31 @@ import { generateImageHoroscope } from '../components/ExportButton.js';
 /**
  * Upon page load, call the init() function
  */
-window.addEventListener("DOMContentLoaded", init);
+window.addEventListener('DOMContentLoaded', init);
 
 async function init() {
   const [theme, mood, advice] = Horoscope.generateReadingFromCurrentUser();
 
-  const items = document.querySelectorAll("horoscope-card");
+  const items = document.querySelectorAll('horoscope-card');
   console.log(items);
-  const labels = ["Theme", "Mood", "Advice"];
+  const labels = ['Theme', 'Mood', 'Advice'];
   const values = [theme, mood, advice];
 
   for (let i = 0; i < labels.length; i++) {
-    items[i].setAttribute("front-title", labels[i]);
-    items[i].setAttribute("back-title", values[i]);
+    items[i].setAttribute('front-title', labels[i]);
+    items[i].setAttribute('back-title', values[i]);
   }
 
   console.log(items);
 
-  const userData = JSON.parse(localStorage.getItem("tarotUserInfo"));
+  const userData = JSON.parse(localStorage.getItem('tarotUserInfo'));
   const sign = Horoscope.getHoroscope(Horoscope.getDate(userData.dob));
-  const signText = document.querySelector(".sign-text");
+  const signText = document.querySelector('.sign-text');
   signText.textContent = sign;
 
-  document.querySelector("#username").textContent = `Hi ${userData.name}!`;
-  document.querySelector("#zodiac-picture").src = `../images/zodiacs/${sign}.png`;
+  document.querySelector('#username').textContent = `Hi ${userData.name}!`;
+  document.querySelector('#zodiac-picture').src =
+    `../images/zodiacs/${sign}.png`;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
