0dcce3045180718ef3cb4bd46f2307a645538821
ominjoo <o.minjoo14@gmail.com>
2025-06-07 19:10:26 -0700

    frontend: horoscope page enhancement

--- source/frontend/DailyHoroscopePage/horoscopePage.js
+++ source/frontend/DailyHoroscopePage/horoscopePage.js
@@ -1,30 +1,31 @@
-import Horoscope from '../../backend/horoscope.js';
+import Horoscope from "../../backend/horoscope.js";
 
 /**
  * Upon page load, call the init() function
  */
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 async function init() {
   const [theme, mood, advice] = Horoscope.generateReadingFromCurrentUser();
 
-  const items = document.querySelectorAll('.item-container');
-  const labels = ['Theme', 'Mood', 'Advice'];
+  const items = document.querySelectorAll("horoscope-card");
+  console.log(items);
+  const labels = ["Theme", "Mood", "Advice"];
   const values = [theme, mood, advice];
 
   for (let i = 0; i < labels.length; i++) {
-    const label = items[i].querySelector('.item-label');
-    const text = items[i].querySelector('.item-text');
-
-    label.textContent = labels[i];
-    text.textContent = values[i];
+    items[i].setAttribute("front-title", labels[i]);
+    items[i].setAttribute("back-title", values[i]);
   }
 
-  const userData = JSON.parse(localStorage.getItem('tarotUserInfo'));
+  console.log(items);
+
+  const userData = JSON.parse(localStorage.getItem("tarotUserInfo"));
   const sign = Horoscope.getHoroscope(Horoscope.getDate(userData.dob));
-  const signText = document.querySelector('.sign-text');
+  const signText = document.querySelector(".sign-text");
   signText.textContent = sign;
 
-  document.querySelector('#username').textContent =
-    `Hi ${userData.name}! Here's today's horoscope reading:`;
+  document.querySelector("#username").textContent = `Hi ${userData.name}!`;
+  // document.querySelector("#zodiac-picture").src = `../images/zodiacs/${sign}.png`;
+  document.querySelector("#zodiac-picture").src = "../images/logo.png";
 }
