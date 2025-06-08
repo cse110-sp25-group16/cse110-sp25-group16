2444e0392234812795ff86501cd05860e2b78323
Tyler <tylerhoang456123@gmail.com>
2025-06-08 02:06:02 -0700

    run prettier

--- source/frontend/DailySpreadPage/dailyspread.js
+++ source/frontend/DailySpreadPage/dailyspread.js
@@ -1,36 +1,36 @@
-import CardDeck from "../../backend/CardDeck.js";
-import Card from "../../backend/Card.js";
-import { generateImageCards } from "../components/ExportButton.js";
-import { getStoredCards } from "../components/ExportButton.js";
+import CardDeck from '../../backend/CardDeck.js';
+import Card from '../../backend/Card.js';
+import { generateImageCards } from '../components/ExportButton.js';
+import { getStoredCards } from '../components/ExportButton.js';
 
 /**
  * Upon page load, call the init() function
  */
-window.addEventListener("DOMContentLoaded", init);
+window.addEventListener('DOMContentLoaded', init);
 
 /**
  * This function serves as the main function for the Dail spread page.
  */
 async function init() {
-  const userData = JSON.parse(localStorage.getItem("tarotUserInfo"));
-  const name = userData.name || "User";
-  document.querySelector("#username").textContent = `Hi ${name}!`;
+  const userData = JSON.parse(localStorage.getItem('tarotUserInfo'));
+  const name = userData.name || 'User';
+  document.querySelector('#username').textContent = `Hi ${name}!`;
 
   /*Selects grid-container in html and clears the cards loaded from previous draw*/
-  const grid = document.querySelector(".cards-container");
-  const selectedAmnt = document.querySelector("#card-amount");
-  console.log("selectedAmnt: " + selectedAmnt.value);
+  const grid = document.querySelector('.cards-container');
+  const selectedAmnt = document.querySelector('#card-amount');
+  console.log('selectedAmnt: ' + selectedAmnt.value);
 
   document
-    .getElementById("generateTarotCardsBtn")
-    .addEventListener("click", () =>
+    .getElementById('generateTarotCardsBtn')
+    .addEventListener('click', () =>
       generateImageCards(getStoredCards(selectedAmnt.value))
     );
 
   /*Selects cards and amount drawn depends on what option is picked */
-  selectedAmnt.addEventListener("change", (event) => {
-    const currDate = new Date().toISOString().split("T")[0]; // gets date in YYYY-MM-DD format
-    const existingData = JSON.parse(localStorage.getItem("dailyCards")) || {};
+  selectedAmnt.addEventListener('change', (event) => {
+    const currDate = new Date().toISOString().split('T')[0]; // gets date in YYYY-MM-DD format
+    const existingData = JSON.parse(localStorage.getItem('dailyCards')) || {};
     let pulledCards;
 
     /**
@@ -56,41 +56,41 @@ async function init() {
         upsideDown: card.upsideDown,
       }));
 
-      localStorage.setItem("dailyCards", JSON.stringify(existingData)); // save back to localStorage
+      localStorage.setItem('dailyCards', JSON.stringify(existingData)); // save back to localStorage
     }
 
     /*Clears amount of card displayed from previous selection */
-    grid.innerHTML = "";
+    grid.innerHTML = '';
 
     /*For each card drawn, create a card webcomponent to then append and display in dailspread.html file */
     for (let i = 0; i < pulledCards.length; i++) {
       if (!pulledCards[i]) continue;
-      const cardElement = document.createElement("card-component");
+      const cardElement = document.createElement('card-component');
 
       cardElement.setAttribute(
-        "image",
+        'image',
         `/source/cards/${pulledCards[i].getImg()}`
       );
-      cardElement.setAttribute("name", pulledCards[i].getCardName());
-      cardElement.setAttribute("arcana", pulledCards[i].getArcana());
-      cardElement.setAttribute("suit", pulledCards[i].getSuit());
+      cardElement.setAttribute('name', pulledCards[i].getCardName());
+      cardElement.setAttribute('arcana', pulledCards[i].getArcana());
+      cardElement.setAttribute('suit', pulledCards[i].getSuit());
       cardElement.setAttribute(
-        "uprightMeanings",
+        'uprightMeanings',
         JSON.stringify(pulledCards[i].getUprightMeanings())
       );
       cardElement.setAttribute(
-        "reversedMeanings",
+        'reversedMeanings',
         JSON.stringify(pulledCards[i].getReversedMeaning())
       );
       cardElement.setAttribute(
-        "keywords",
+        'keywords',
         JSON.stringify(pulledCards[i].getKeywords())
       );
-      cardElement.setAttribute("symbolism", pulledCards[i].getSymbolism());
-      cardElement.setAttribute("description", pulledCards[i].getDescription());
-      cardElement.setAttribute("numeral", pulledCards[i].getNumeral());
-      cardElement.setAttribute("facing", pulledCards[i].isFaceUp());
-      cardElement.setAttribute("upsideDown", pulledCards[i].isUpsideDown());
+      cardElement.setAttribute('symbolism', pulledCards[i].getSymbolism());
+      cardElement.setAttribute('description', pulledCards[i].getDescription());
+      cardElement.setAttribute('numeral', pulledCards[i].getNumeral());
+      cardElement.setAttribute('facing', pulledCards[i].isFaceUp());
+      cardElement.setAttribute('upsideDown', pulledCards[i].isUpsideDown());
 
       grid.appendChild(cardElement);
     }
