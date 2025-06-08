41d2e6eeeb945527d2c640e272c9e1d16edf3920
LeoJuJu05090822 <103226676+LeoJuJu05090822@users.noreply.github.com>
2025-06-08 16:44:02 -0700

    varibale name comflict

--- __tests__/unit/ExportButton.test.js
+++ __tests__/unit/ExportButton.test.js
@@ -1,5 +1,5 @@
 import { jest } from '@jest/globals';
-import * as ExportButton from '../../source/frontend/components/ExportButton.js';
+import * as ExportBtn from '../../source/frontend/components/ExportButton.js';
 
 // Mock Card class
 jest.mock('../../source/backend/Card.js', () => {
