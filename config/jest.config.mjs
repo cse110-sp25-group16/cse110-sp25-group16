429dc8dbe10180815ac26b7ae60476b2a467a869
Tyler <tylerhoang456123@gmail.com>
2025-06-08 23:52:56 -0700

    fix: wrong pathing for jest

--- config/jest.config.mjs
+++ config/jest.config.mjs
@@ -5,7 +5,7 @@ export default {
   verbose: true,
 
   testPathIgnorePatterns: ['__tests__/e2e/'],
-  coveragePathIgnorePatterns: ['__tests__/e2e/'],
+  coveragePathIgnorePatterns: ['__tests__/e2e/', 'source/frontend'],
 
   testMatch: ['<rootDir>/__tests__/**/*.test.js'],
 };
